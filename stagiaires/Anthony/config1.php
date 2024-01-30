e1d5a68a45e0137fba3590465ae6711ecb469910
Anthoonyyy <Anthony212007@hotmail.com>
2024-01-30 16:09:18 +0100

    exemple01

--- stagiaires/Anthony/config1.php
+++ stagiaires/Anthony/config1.php
@@ -1,11 +1,11 @@
 <?php
 // paramètres de connexions
-const DB_HOST = "localhost";
-const DB_LOGIN = "root";
-const DB_PWD = "";
-const DB_NAME = "listepays";
-const DB_PORT = 3307;
-const DB_CHARSET = "utf8mb4";
+const DB_HOST = "localhost";     //Le chemin vers le serveur (souvent localhost)
+const DB_LOGIN = "root";        //Nom d'utilisateur pour se conecter
+const DB_PWD = "";               //Mot de passe pour se connecter
+const DB_NAME = "listepays";    //Nom de la base de données
+const DB_PORT = 3307;           //Chemin vers le port
+const DB_CHARSET = "utf8mb4";   //charset
 
 // paramètres supplémentaires pour PDO
 const DB_TYPE = "mysql"; // valable pour MySQL et/ou MariaDB
