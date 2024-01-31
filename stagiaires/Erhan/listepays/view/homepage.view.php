bdec244708281e78f47a6750c5be061be89baef3
erdaserhan <erhan209@gmail.com>
2024-01-31 11:26:35 +0100

    homepage.view

---
+++ stagiaires/Erhan/listepays/view/homepage.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listedepays</title>
+</head>
+<body>
+    <h1>Listepays</h1>
+    <h2>Liste de tous les pays du monde</h2>
+    <h3>Nombre de pays : <?=$countQuery?></h3>
+    <p><code>Utilisation du while sur un PDOStatement</code></p>
+    <?php var_dump($query);?>
+</body>
+</html>
