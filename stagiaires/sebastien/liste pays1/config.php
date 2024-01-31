8b29b0ab75cc2d78634dfedb68e552b5162fcd6f
sebastien2506 <jestrovic_8@msn.com>
2024-01-31 16:18:12 +0100

    e

---
+++ stagiaires/sebastien/liste pays1/config.php
@@ -0,0 +1,10 @@
+<?php
+// creation des constantes de connection 
+
+const MY_DB_DRIVER = "mysql";  //   driver
+const MY_DB_HOST = "localhost"; // chemin,ip,
+const MY_DB_LOGIN = "root";  //  username
+const MY_DB_PWD = "";   //   password
+const MY_DB_NAME = "liste_pays";   //   base de donnee mysql
+const MY_DB_PORT = 3306; // mysql   3307 maria db
+const MY_DB_CHARSET = "utf8mb4";   //   encodage 
---
+++ stagiaires/sebastien/liste pays1/readme.md
@@ -0,0 +1,6 @@
+# listepays
+
+## instalation
+
+- Renommez 'config.php.ini' en ' cinfig.php' et entrez les parametres de votre base de donnee.
+- Importez le fichier 
