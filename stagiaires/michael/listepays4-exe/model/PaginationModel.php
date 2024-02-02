b865ceb9c1310da04aa8a38c44513f0e8ce6d138
mikhawa <michaeljpitz@gmail.com>
2024-02-02 09:05:48 +0100

    pagination ok

--- stagiaires/michael/listepays4-exe/model/PaginationModel.php
+++ stagiaires/michael/listepays4-exe/model/PaginationModel.php
@@ -22,21 +22,30 @@ function PaginationModel(string $url, // url (pour garder les autres variables g
     // si une seule page, pas de pagination
     if($nbPage<2) return null;
 
-    // on commence par le bouton précédent, utilisation d'une ternaire pour remplacer un il et else
-    $sortie.= $currentPage===1
-                ? "<< <"
-                : "<a href='$url'><<</a> <a href='$url?&$getName=".($currentPage-1)."'><</a>";
+    // on commence par le bouton précédent
+    if($currentPage===1){
+        // pas de liens
+        $sortie.= "<< <";
+    }elseif ($currentPage===2) {
+        // liens vers l'accueil
+        $sortie.= "<a href='$url'><<</a> <a href='$url'><</a>";
+    }else{
+        // liens vers l'accueil et la page précédente
+        $sortie.= "<a href='$url'><<</a> <a href='$url?&$getName=".($currentPage-1)."'><</a>";
+    }
 
     // on boucle sur le nombre de pages
     for($i=1;$i<=$nbPage;$i++)
     {
         // si on est sur la page en cours, on affiche un texte
         if($i===$currentPage) $sortie.= " $i ";
+        // sinon si on affiche la page 1
+        else if($i===1) $sortie.= " <a href='$url'>$i</a> ";
         // sinon on affiche un lien
         else $sortie.= " <a href='$url?&$getName=$i'>$i</a> ";
     }
 
-    // on termine par le bouton suivant
+    // on termine par le bouton suivant, utilisation d'une ternaire pour remplacer un il et else
     $sortie.= $currentPage === $nbPage ? "> >>" : "<a href='$url?&$getName=".($currentPage+1)."'>></a> <a href='$url?&$getName=$nbPage'>>></a>";
 
     return $sortie;
